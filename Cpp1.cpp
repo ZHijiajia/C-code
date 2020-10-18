30a710ff83ee7db302328d53124bff76a8d65e61
ZHijiajia <72651583+ZHijiajia@users.noreply.github.com>
2020-10-18 15:46:54 +0800

    if
    if

--- Cpp1.cpp
+++ Cpp1.cpp
@@ -2,22 +2,14 @@
 int main()
 {
     int x,y;
-	printf(" ‰»Îx:");
+	printf("enter x:");
 	scanf("%d",&x);
-	if(x<1)     //x<1
-	{
-	   y=x;
-	   printf("x=%3d,y=x=%d\n",x,y);
-	}
-    else if(x<10)       //1=<x<10
-	{
-	   y=2*x-1;
-	   printf("x=%d,y=2*x-1=%d\n",x,y);
-	}
-	else   //x>=10
-	{
-	   y=3*x-11;
-	   printf("x=%d,y=3*x-11=%d\n",x,y);
-	}
-	return 0;
+	y=-1;
+	if(x!=0)
+		if(x>0)
+			y=1;
+		else
+			y=0;
+		printf("x=%d,y=%d\n",x,y);
+		return 0;
 }
