6c91b00f2275fb71cbcc57deae80b065818770ee
ZHijiajia <72651583+ZHijiajia@users.noreply.github.com>
2020-10-18 15:31:27 +0800

    piecewise function
    Cylinder piecewise function

--- Cpp1.cpp
+++ Cpp1.cpp
@@ -1,12 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 int main()
 {
-	float p,r,n;
-	r=0.07;
-	n=10;
-	p=pow(1+r,n);
-	printf("p=%f\n",p);
+    int x,y;
+	printf(" ‰»Îx:");
+	scanf("%d",&x);
+	if(x<1)     //x<1
+	{
+	   y=x;
+	   printf("x=%3d,y=x=%d\n",x,y);
+	}
+    else if(x<10)       //1=<x<10
+	{
+	   y=2*x-1;
+	   printf("x=%d,y=2*x-1=%d\n",x,y);
+	}
+	else   //x>=10
+	{
+	   y=3*x-11;
+	   printf("x=%d,y=3*x-11=%d\n",x,y);
+	}
 	return 0;
-
 }
